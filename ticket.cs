9848586c52c610768e530ce9c4cfb4acfc6fa0ba
Lisa Bost <bost.lisam@gmail.com>
2021-02-22 12:36:33 -0600

    Added getters and setters

--- ticket.cs
+++ ticket.cs
@@ -10,12 +10,34 @@ namespace ticketingSystem
         private string _status;
         private string _priority;
         private string _submitter;
-        private string[] _watching; 
-        
+        private string[] _watching;
+
         //constructor
         public ticket()
         {
 
+        }
+
+        //methods
+        public string Summary
+        {
+            get; set;
+        }
+        public string Status
+        {
+            get; set;
+        }
+        public string Priortiy
+        {
+            get; set;
+        }
+        public string Submitter
+        {
+            get; set;
+        }
+        public string[] Watcher
+        {
+            get; set;
         }
     }
 
