93a35f7e305100955da7e46b6d59291f1b197438
Lisa Bost <bost.lisam@gmail.com>
2021-02-22 14:49:00 -0600

    added in assigned parameter

--- FileReader.cs
+++ FileReader.cs
@@ -33,7 +33,7 @@ namespace ticketingSystem
             {
                 string line = sr.ReadLine();
                 string[] arr = line.Split(',');
-                ticketsFromFile.Add(new Ticket(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]));
+                ticketsFromFile.Add(new Ticket(int.Parse(arr[0]), arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]));
             }
             return ticketsFromFile;
         }
--- ticket.cs
+++ ticket.cs
@@ -12,22 +12,28 @@ namespace ticketingSystem
         private string _status;
         private string _priority;
         private string _submitter;
+        private string _assigned;
         private string _watching;
 
         //constructor
         public Ticket(){}
 
-        public Ticket(string ticketNum, string summary, string status, string priority, string submitter, string watching)
+        public Ticket(int ticketNum, string summary, string status, string priority, string submitter, string assigned, string watching)
         {
-            this._ticketNum = int.Parse(ticketNum);
+            this._ticketNum = ticketNum;
             this._summary = summary;
             this._status = status;
             this._priority = priority;
             this._submitter = submitter;
+            this._assigned = assigned;
             this._watching = watching;
         }
 
         //methods - getters and setters
+        public string ticketNum
+        {
+            get; set;
+        }
         public string Summary
         {
             get; set;
@@ -44,7 +50,7 @@ namespace ticketingSystem
         {
             get; set;
         }
-        public string Watcher
+        public string[] Watching
         {
             get; set;
         }
@@ -53,7 +59,8 @@ namespace ticketingSystem
         public override string ToString()
         {
             return "Ticket Number:" + this._ticketNum + ", Summary: " + this._summary + ", Status: " 
-            + this._status + ", Priority: " + this._priority + ", Submitter: " + this._submitter + ", Watching: " + this._watching;
+            + this._status + ", Priority: " + this._priority + ", Submitter: " +  this._submitter + ", Assigned: " 
+            + this._assigned + ", Watching: " + this._watching;
         }
     }
 
