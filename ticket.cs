b4df0a7a35dd7934a2004d71a0c1ebd0e4d761e3
Lisa Bost <bost.lisam@gmail.com>
2021-02-22 12:35:00 -0600

    added ticket class with fields and constructor

---
+++ ticket.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+
+namespace ticketingSystem
+{
+    class ticket
+    {
+        //ticket fields
+        private string _summary;
+        private string _status;
+        private string _priority;
+        private string _submitter;
+        private string[] _watching; 
+        
+        //constructor
+        public ticket()
+        {
+
+        }
+    }
+
+}
