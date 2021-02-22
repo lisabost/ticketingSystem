a979a55be1542fa1c3dd02129a1432f5ab1d9d6b
Lisa Bost <bost.lisam@gmail.com>
2021-02-22 13:49:22 -0600

    Added constructor that takes parameters

--- ticket.cs
+++ ticket.cs
@@ -7,17 +7,24 @@ namespace ticketingSystem
     class Ticket
     {
         //ticket fields set to private
-        private string _ticketNum;
+        private int _ticketNum;
         private string _summary;
         private string _status;
         private string _priority;
         private string _submitter;
-        private string[] _watching;
+        private string _watching;
 
         //constructor
-        public Ticket()
-        {
+        public Ticket(){}
 
+        public Ticket(string ticketNum, string summary, string status, string priority, string submitter, string watching)
+        {
+            this._ticketNum = int.Parse(ticketNum);
+            this._summary = summary;
+            this._status = status;
+            this._priority = priority;
+            this._submitter = submitter;
+            this._watching = watching;
         }
 
         //methods
@@ -37,7 +44,7 @@ namespace ticketingSystem
         {
             get; set;
         }
-        public string[] Watcher
+        public string Watcher
         {
             get; set;
         }
