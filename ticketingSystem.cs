2fdba9637726a212c982ca970df90938605f3abc
Lisa Bost <bost.lisam@gmail.com>
2021-02-22 12:47:29 -0600

    Added class ticket list

---
+++ ticketingSystem.cs
@@ -0,0 +1,11 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ticketingSystem
+{
+    class ticketingSystem
+    {
+        private List<string> ticketList;
+    }
+}
