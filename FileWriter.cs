05acb9424c781d489cb3b99596e85a42340109c3
Lisa Bost <bost.lisam@gmail.com>
2021-02-22 15:31:07 -0600

    create class to write our ticket to the file

--- FileWriter.cs
+++ FileWriter.cs
@@ -4,5 +4,23 @@ using System.Collections.Generic;
 
 namespace ticketingSystem
 {
-    
+    /*
+    This class is going to write to our file
+    */
+    class FileWriter
+    {
+        //fields
+        private static string _filepath = "tickets.txt";
+
+        //constructor
+        public FileWriter(){}
+
+        //methods
+        public void WriteToFile(Ticket input)
+        {
+            StreamWriter sw = new StreamWriter(_filepath, append: true);
+            sw.WriteLine($"{input.GetTicketNum()},{input.GetSummary()},{input.GetStatus()},{input.GetPriority()},{input.GetSubmitter()},{input.GetAssigned()},{input.GetWatching()}");
+            sw.Close();
+        }
+    }
 }
