6287725e2679dd6e320fd232c2e1f4149de4d412
Lisa Bost <bost.lisam@gmail.com>
2021-03-01 12:35:37 -0600

    Added a screen writer class to control output to console

---
+++ ScreenWriter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ticketingSystem
+{
+    class ScreenWriter
+    {
+        public ScreenWriter() { }
+
+        public WriteToScreen(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
