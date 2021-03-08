d90251d58b745692adde6ad44a8c529ddb8219dd
Lisa Bost <bost.lisam@gmail.com>
2021-03-08 09:24:20 -0600

    changed total lines method to accept a file as a parameter

--- FileReader.cs
+++ FileReader.cs
@@ -13,9 +13,9 @@ namespace ticketingSystem
         private static string _filepath = "tickets.txt";
 
         //method to find the total number of lines in the file
-        public int TotalLines()
+        public int TotalLines(string filePath)
         {
-            StreamReader sr = new StreamReader(_filepath);
+            StreamReader sr = new StreamReader(filePath);
             using (sr)
             {
                 int i = 0;
@@ -34,7 +34,13 @@ namespace ticketingSystem
             {
                 string line = sr.ReadLine();
                 string[] arr = line.Split(',');
-                ticketsFromFile.Add(new Ticket(int.Parse(arr[0]), arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]));
+                string[] watching = arr[6].Split("|");
+                List<string> watchers = new List<string>();
+                foreach (var item in watching)
+                {
+                    watchers.Add(item);
+                }
+                ticketsFromFile.Add(new BugDefect(int.Parse(arr[0]), arr[1], arr[2], arr[3], arr[4], arr[5], watchers, arr[7]));
             }
             sr.Close();
             return ticketsFromFile;
