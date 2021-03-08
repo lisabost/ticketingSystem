56aebb6e801e9d432584edaa51b41292a52372ad
Lisa Bost <bost.lisam@gmail.com>
2021-03-08 09:44:43 -0600

    added methods to read the different files

--- FileReader.cs
+++ FileReader.cs
@@ -26,10 +26,10 @@ namespace ticketingSystem
         }
 
         //method to read the lines already in the file
-        public List<Ticket> ReadAll()
+        public List<Ticket> ReadAllBugs(string filePath)
         {
             List<Ticket> ticketsFromFile = new List<Ticket>();
-            StreamReader sr = new StreamReader(_filepath);
+            StreamReader sr = new StreamReader(filePath);
             while (!sr.EndOfStream)
             {
                 string line = sr.ReadLine();
@@ -41,6 +41,46 @@ namespace ticketingSystem
                     watchers.Add(item);
                 }
                 ticketsFromFile.Add(new BugDefect(int.Parse(arr[0]), arr[1], arr[2], arr[3], arr[4], arr[5], watchers, arr[7]));
+            }
+            sr.Close();
+            return ticketsFromFile;
+        }
+
+        public List<Ticket> ReadAllEnhancements(string filePath)
+        {
+            List<Ticket> ticketsFromFile = new List<Ticket>();
+            StreamReader sr = new StreamReader(filePath);
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] arr = line.Split(",");
+                string[] watching = arr[6].Split("|");
+                List<string> watchers = new List<string>();
+                foreach (var item in watching)
+                {
+                    watchers.Add(item);
+                }
+                ticketsFromFile.Add(new Enhancement(int.Parse(arr[0]), arr[1], arr[2], arr[3], arr[4], arr[5], watchers, arr[7], double.Parse(arr[8]), arr[9], arr[10]));
+            }
+            sr.Close();
+            return ticketsFromFile;
+        }
+
+        public List<Ticket> ReadAllTasks(string filePath)
+        {
+            List<Ticket> ticketsFromFile = new List<Ticket>();
+            StreamReader sr = new StreamReader(filePath);
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] arr = line.Split(",");
+                string[] watching = arr[6].Split("|");
+                List<string> watchers = new List<string>();
+                foreach (var item in watching)
+                {
+                    watchers.Add(item);
+                }
+                ticketsFromFile.Add(new Task(int.Parse(arr[0]), arr[1], arr[2], arr[3], arr[4], arr[5], watchers, arr[7], DateTime.Parse(arr[8])));
             }
             sr.Close();
             return ticketsFromFile;
