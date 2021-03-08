02b890517ad0f357e652cac7d752612c9ddd04d8
Lisa Bost <bost.lisam@gmail.com>
2021-03-08 09:22:04 -0600

    created bug/defect class

---
+++ BugDefect.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using NLog.Web;
+
+namespace ticketingSystem
+{
+    public class BugDefect : Ticket
+    {
+        //fields
+        public string severity { get; set; }
+
+        //constructor
+        public BugDefect()
+        {}
+        public BugDefect(int ticketNum, string summary, string status, string priority, string submitter, string assigned, List<string> watching, string severity)
+        {
+            this.ticketNum = ticketNum;
+            this.summary = summary;
+            this.status = status;
+            this.priority = priority;
+            this.submitter = submitter;
+            this.assigned = assigned;
+            this.watching = watching;
+            this.severity = severity;
+        }
+
+        //methods
+        public override string Display()
+        {
+            return base.Display() + $"Severity: {severity}\n";
+        }
+    }
+}
