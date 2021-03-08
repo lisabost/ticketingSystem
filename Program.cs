8adf31a138dd640060a248a25457a15d265f79d6
Lisa Bost <bost.lisam@gmail.com>
2021-03-08 09:25:01 -0600

    Added logic to make different types of ticket

--- Program.cs
+++ Program.cs
@@ -14,9 +14,7 @@ namespace ticketingSystem
             FileReader fr = new FileReader();
             ConsoleWriter cw = new ConsoleWriter();
             ConsoleReader cr = new ConsoleReader();
-            
-            //get current ticketNum from file based on number of tickets already assinged
-            int ticketNum = fr.TotalLines();
+
             do
             {
                 cw.WriteToScreen("1) Enter a new ticket");
@@ -31,41 +29,92 @@ namespace ticketingSystem
                     FileWriter fw = new FileWriter();
 
                     //find out how many tickets they want to enter
-                    cw.WriteToScreen("How many tickets do you have to enter?");
-                    int ticketNumber = int.Parse(cr.ReadFromConsole());
+                    cw.WriteToScreen("Select the type of ticket.");
+                    cw.WriteToScreen("1) Bug or Defect");
+                    cw.WriteToScreen("2) Enhancement");
+                    cw.WriteToScreen("3) Task");
 
-                    for (int x = 0; x < ticketNumber; x++)
+                    string response = cr.ReadFromConsole();
+
+                    //This stuff is the same for any ticket
+                    cw.WriteToScreen("Enter ticket summary");
+                    string prob = cr.ReadFromConsole();
+                    //status
+                    cw.WriteToScreen("Enter the ticket's status");
+                    string status = cr.ReadFromConsole();
+                    //priortiy
+                    cw.WriteToScreen("Enter the ticket's priorty");
+                    string priority = cr.ReadFromConsole();
+                    //submitter
+                    cw.WriteToScreen("Enter the ticket's submitter");
+                    string sub = cr.ReadFromConsole();
+                    //assigned
+                    cw.WriteToScreen("Who is assigned to this ticket");
+                    string assigned = cr.ReadFromConsole();
+                    //watching - can be multiple people
+                    List<string> watching = new List<string>();
+                    string input;
+                    do
                     {
-                        //assign ticket number
-                        ticketNum++;
-                        //summary
-                        cw.WriteToScreen($"Enter ticket {x + 1} summary");
-                        string prob = cr.ReadFromConsole();
-                        //status
-                        cw.WriteToScreen("Enter the ticket's status");
-                        string status = cr.ReadFromConsole();
-                        //priortiy
-                        cw.WriteToScreen("Enter the ticket's priorty");
-                        string priority = cr.ReadFromConsole();
-                        //submitter
-                        cw.WriteToScreen("Enter the ticket's submitter");
-                        string sub = cr.ReadFromConsole();
-                        //assigned
-                        cw.WriteToScreen("Who is assigned to this ticket");
-                        string assigned = cr.ReadFromConsole();
-                        //watching - can be multiple people
-                        cw.WriteToScreen("How many people are watching this ticket?");
-                        int numWatching = int.Parse(cr.ReadFromConsole());
-                        string[] watching = new string[numWatching];
-                        for (int i = 0; i < numWatching; i++)
+                        cw.WriteToScreen("Enter who is watching (or done to quit)");
+                        input = cr.ReadFromConsole();
+
+                        if (input != "done" && input.Length > 0)
                         {
-                            cw.WriteToScreen("Who is watching this ticket");
-                            watching[i] = cr.ReadFromConsole();
+                            watching.Add(input);
                         }
-                        //make a ticket
-                        Ticket ticket = new Ticket(ticketNum++, prob, status, priority, sub, assigned, string.Join("|", watching));
-                        fw.WriteToFile(ticket);
+                    } while (input != "done");
+
+                    //Bug/Defect Ticket
+                    if (response == "1")
+                    {
+                        //bug tickets need severity - their ticket numbers come from the tickets.txt file
+                        int ticketNumber = fr.TotalLines("tickets.txt") + 1;
+
+                        cw.WriteToScreen("Enter severity of bug or defect");
+                        string severity = cr.ReadFromConsole();
+
+                        //make our ticket and write it to the file
+                        fw.WriteBugToFile(new BugDefect(ticketNumber, prob, status, priority, sub, assigned, watching, severity));
                     }
+                    //Enhancement Ticket
+                    else if (response == "2")
+                    {
+                        //enhancement tickets need software, cost, reason, estimate - their ticket numbers come from Enhancements.txt file
+                        int ticketNumber = fr.TotalLines("Enhancements.txt") + 1;
+
+                        cw.WriteToScreen("Enter software needed");
+                        string software = cr.ReadFromConsole();
+
+                        cw.WriteToScreen("Enter cost");
+                        double cost = double.Parse(cr.ReadFromConsole());
+
+                        cw.WriteToScreen("Enter reason");
+                        string reason = cr.ReadFromConsole();
+
+                        cw.WriteToScreen("Enter estimate");
+                        string estimate = cr.ReadFromConsole();
+
+                        //make our ticket and write it to the file
+                        fw.WriteEnhancementToFile(new Enhancement(ticketNumber, prob, status, priority, sub, assigned, watching, software, cost, reason, estimate));
+                    }
+                    //Task ticket
+                    else if (response == "3")
+                    {
+                        //task tickets need project name and due date - their ticket numbers come from the Tasks.txt file
+                        int ticketNumber = fr.TotalLines("Tasks.txt");
+
+                        cw.WriteToScreen("Enter the project name");
+                        string projectName = cr.ReadFromConsole();
+
+                        cw.WriteToScreen("Enter the Due Date as mm/dd/yyyy");
+                        DateTime dueDate = DateTime.Parse(cr.ReadFromConsole());
+
+                        //make our ticket and write it to the file
+                    }
+
+
+
                 }
                 else if (choice == "2")
                 {
@@ -76,7 +125,7 @@ namespace ticketingSystem
                         List<Ticket> ticketsInFile = fr.ReadAll();
                         foreach (var item in ticketsInFile)
                         {
-                            cw.WriteToScreen(item.ToString());
+                            cw.WriteToScreen(item.Display());
                         }
                     }
                     else
